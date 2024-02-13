3928e056821b7cb86dff5a5e0b232df1551252cc
wathika-eng <wathika02@gmail.com>
2024-02-13 22:37:57 +0300

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,72 +1,34 @@
 #!/usr/bin/python3
-"""Calculate area and perimeter of a square"""
+""" Module for square class"""
 
 
-class square:
-    """
-    Represents a square with specified width and height.
+class Square:
+    """Square class"""
 
-    Attributes:
-        width (int): The width of the square.
-        height (int): The height of the square.
+    width = 0
+    height = 0
 
-    Methods:
-        __init__(self, width=0, height=0):
-            Initializes a Square object with the given width and height.
-
-        area(self) -> int:
-            Calculates and returns the area of the square.
-
-        perimeter(self) -> int:
-            Calculates and returns the perimeter of the square.
-
-        __str__(self) -> str:
-            Returns a string representation of the square, including its width and height.
-    """
-
-    def __init__(self, width=0, height=0):
-        """
-        Initializes a Square object with the given width and height.
-
-        Args:
-            width (int): The width of the square.
-            height (int): The height of the square.
-        """
-        self.width = width
-        self.height = height
+    def __init__(self, *args, **kwargs):
+        """Instantiation of class"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """
-        Calculates and returns the area of the square.
+        """Area of the square"""
+        return self.width * self.height
 
-        Returns:
-            int: The area of the square.
-        """
-        return self.width * self.width
-
-    def PerimeterOfMySquare(self):
-        """
-        Calculates and returns the perimeter of the square.
-
-        Returns:
-            int: The perimeter of the square.
-        """
-        return 4 * self.width
+    def permiter_of_my_square(self):
+        """Perimeter of my square"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """
-        Returns a string representation of the square, including its width and height.
-
-        Returns:
-            str: A string representation of the square.
-        """
-
-        return f"{self.width}, {self.height}"
+        """Printable representation"""
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    """Create a square object"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print(s.permiter_of_my_square())
